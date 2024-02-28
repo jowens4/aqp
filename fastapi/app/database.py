c12e8df88e233090fb2dcc1ade7244b0d87cdc14
jowens4 <jowens@avidpd.com>
2024-02-28 14:47:16 -0700

    got default db connection working

--- fastapi/app/database.py
+++ fastapi/app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@0.0.0.0:5432/quality-db"
-#database_url = "postgresql://username:password@localhost/database_name"
+SQLALCHEMY_DATABASE_URL = "postgresql://aqp:aqp@postgres:5432/aqp"
+#database_url = "postgresql://username:password@localhost:port/database_name"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
