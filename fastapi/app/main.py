4d43a40fb611e89c74efe5748d083a19befbdd82
jowens4 <jowens@avidpd.com>
2024-06-10 16:47:25 -0600

    upload v

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -73,3 +73,7 @@ def create_item_for_user(
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
+
+
+@app.get("/ips/")
+def get_ips():
